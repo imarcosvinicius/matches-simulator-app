6d3d53098e94b4ce28e0ec385480eb8b4e08a179
Marcos Vinicius Nunes <imarcosvinicius@outlook.com>
2022-06-08 20:32:06 -0300

    create new activity

---
+++ app/src/main/java/io/myapps/simulator/DetailActivity.kt
@@ -0,0 +1,17 @@
+package io.myapps.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.myapps.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
