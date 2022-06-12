fa2bddb85c27dd5bc358ce4f5cae50bf196f953c
Marcos Vinicius Nunes <imarcosvinicius@outlook.com>
2022-06-12 09:08:50 -0300

    Implementado Retrofit2 e SerializedName

--- app/src/main/java/io/myapps/simulator/domain/Matches.kt
+++
@@ -1,8 +0,0 @@
-package io.myapps.simulator.domain
-
-data class Matches(
-    val description: String,
-    val place: Place,
-    val homeTeam: Team,
-    val awayTeam: Team
-)
--- app/src/main/java/io/myapps/simulator/domain/Team.kt
+++ app/src/main/java/io/myapps/simulator/domain/Team.kt
@@ -1,7 +1,13 @@
 package io.myapps.simulator.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class Team(
+
+    @SerializedName("nome")
     val name: String,
+    @SerializedName("forca")
     val stars: Int,
+    @SerializedName("imagem")
     val image: String
 )
